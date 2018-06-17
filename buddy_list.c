e31e16e0eafd6df027bfc57651a931e6b8971b52
pablofsf <ablocorrea@hotmail.com>
2018-06-17 23:03:00 +0200

    Added debugging symbols and fixed small mistakes

--- buddy_list.c
+++ buddy_list.c
@@ -1,28 +1,51 @@
 #include "buddy_list.h"
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
-#define MIN_ALLOC ((size_t)1 << 4)
-#define MAX_ALLOC ((size_t)1 << 16)
-#define DATA_SIZES 13
-#define SIZE_HEAP_DATA ((void *) sizeof(struct heap_data))
+#define MIN_ALLOC_BITS 5
+#define MAX_ALLOC_BITS 20
+#define MIN_ALLOC ((size_t)1 << MIN_ALLOC_BITS)
+#define MAX_ALLOC ((size_t)1 << MAX_ALLOC_BITS)
+#define DATA_SIZES (MAX_ALLOC_BITS - MIN_ALLOC_BITS + 1)
+#define SIZE_HEAP_DATA (sizeof(struct heap_data))
 
+#define ABUG
 static void merge(struct heap_data *h1,struct heap_data *h2,int index);
 static void free_block(struct heap_data *h,int index);
 
 
 static struct heap_data* freelist[DATA_SIZES];
-static struct heap_data head;
-static void *startmem;
+static struct heap_data* head;
 
 static int get_index(size_t size)
 {
 	int i;
-    for(i = DATA_SIZES - 1; i >= 0; i--){
-		if(size >= (1 << (i + 3))){
+	for(i = DATA_SIZES - 1; i >= 0; i--){
+		if(size > (1 << (i + MIN_ALLOC_BITS - 1))){
 			break;
 		}
 	}
 	return i;
+}
+
+static void printlist(){
+	for(int i = DATA_SIZES - 1; i >= 0; i--){
+		if(freelist[i] == NULL){
+			fprintf(stderr, "%i: Empty\n",i);
+		}else{
+			struct heap_data *temp = freelist[i];
+			fprintf(stderr,"%i: ",i);
+			while(temp != NULL){
+				fprintf(stderr, "%p ",(void *) temp);
+				temp = temp->next;
+			}
+			fprintf(stderr, "\n");
+
+		}
+	}
+	fprintf(stderr, "\n");
+
 }
 
 static void add_node(struct heap_data *h,int index)
@@ -60,46 +83,53 @@ static void remove_node(struct heap_data *h,int index)
 }
 
 void init(){
-	startmem = sbrk(MAX_ALLOC+sizeof(struct heap_data));
-	if(startmem == (void*) -1){
-		perror(errno);
+	head = sbrk(MAX_ALLOC+sizeof(struct heap_data));
+	if(head == (void*) -1){
+		perror("Cannot allocate memory");
 	}
 
-	head.size = MAX_ALLOC;
-	head.next = NULL;
-	head.prev = NULL;
+	head->size = MAX_ALLOC;
+	head->next = NULL;
+	head->prev = NULL;
 
-	freelist[12] = &head;
+	freelist[DATA_SIZES - 1] = head;
 
 }
 
 void *malloc(size_t size){
-
-	if(startmem == NULL){
+#ifdef ABUG
+	fprintf(stderr, "We are in malloc: %lu\n",size);
+#endif
+	if(head == NULL){
 		init();
 	}
 
-	if(size > MAX_ALLOC || size == 0){
+	if(size > (MAX_ALLOC - SIZE_HEAP_DATA) || size == 0){
 		return NULL;
 	}
 
-	int req = get_index(size);
+	int req = get_index(size + SIZE_HEAP_DATA);
 
 	int available = req;
 
 	struct heap_data *temphead = freelist[req];
 
-	while(temphead == NULL && available < 13){
+//FIX, position of available
+	while(temphead == NULL && available < (DATA_SIZES - 1)){
 		available++;
 		temphead = freelist[available];
 	}
 
 	if(temphead == NULL){
+		fprintf(stderr,"Not enough memory\n");
 		return NULL;
 	}
 
 	while(available > req){
-
+#ifdef ABUG
+		fprintf(stderr,"available: %i\n",available);
+		printlist();
+#endif
 		struct heap_data *parent = freelist[available];
 		freelist[available] = parent->next;
 
@@ -110,9 +140,9 @@ void *malloc(size_t size){
 		available--;
 
 		struct heap_data *buddy1 = parent;
-		struct heap_data *buddy2 = ((size_t) parent) + (parent->size >> 1);
+		struct heap_data *buddy2 = (struct heap_data *) ((size_t) parent + (parent->size >> 1));
 
-		buddy1->size = buddy2->size = () + MIN_ALLOC;
+		buddy1->size = buddy2->size = 1 << (available + MIN_ALLOC_BITS);
 		buddy1->prev = NULL;
 		buddy2->prev = buddy1;
 		buddy1->next = buddy2;
@@ -128,23 +158,19 @@ void *malloc(size_t size){
 		new_parent->next->prev = NULL;
 		new_parent->prev = NULL;
 	}
-
+#ifdef ABUG
 	printlist();
-
+	fprintf(stderr,"return %p, %lu\n",(void *) ((size_t) new_parent + sizeof(struct heap_data)),size);
+#endif
 	return (void *) ((size_t) new_parent + sizeof(struct heap_data));
 }
 
 static void *contiguous(struct heap_data *h,size_t size)
 {
-	size_t addr = ((size_t) h) - (size_t) startmem;
-	size_t base, offset;
-	base = addr / (size << 1);
-	offset = addr % (size << 1);
+	size_t addr = ((size_t) h) - (size_t) head;
+	size_t buddy = addr ^ (size);
 
-	if (offset == 0)
-		return (void *) (base + size);
-	else
-		return (void *) (base);
+	return (void *) (buddy + ((size_t) head));
 }
 
 static void free_block(struct heap_data *h,int index)
@@ -184,26 +210,67 @@ static void merge(struct heap_data *h1,struct heap_data *h2,int index)
 
 void free(void *ptr)
 {
-	struct heap_data *h = (struct heap_data *) (ptr - SIZE_HEAP_DATA);
+#ifdef ABUG
+	fprintf(stderr,"We are in free: %p\n",ptr);
+#endif
+	//Fix, pointer null
+	if (ptr == NULL)
+		return;
+	
+	struct heap_data *h = (struct heap_data *) ((size_t) ptr - SIZE_HEAP_DATA);
 	int index = get_index(h->size);
 
 	h->next = freelist[index];
 	freelist[index] = h;
 	free_block(h,index);
+#ifdef ABUG
+	printlist();
+#endif
 }
 
-void printlist(){
-	for(int i = 12; i > 0; i--){
-		if(freelist[i] == NULL){
-			fprintf(stderr, "Not created \n");
-		}else{
-			struct heap_data *temp = freelist[i];
-			while(temp != NULL){
-				fprintf(stderr, "size: %lu ",temp->size);
-				temp = temp->next;
-		}
-	}
-	fprintf(stderr, "\n");
+void *calloc(size_t nmemb, size_t size)
+{
+#ifdef ABUG
+	fprintf(stderr, "We are in calloc:\n");
+#endif
+	void *ptr;
+
+	ptr =  malloc(nmemb*size);
+	if (ptr)
+		memset(ptr,'\0',nmemb*size);
+	return ptr;
+}
+
+void *realloc(void *ptr, size_t size)
+{
+#ifdef ABUG
+	fprintf(stderr, "We are in realloc: %p, %lu\n",ptr,size);
+#endif
+	void *tmp;
+	struct heap_data *h;
+
+
+	if (ptr == NULL) {
+		return malloc(size);
 	}
 
+	h = (struct heap_data *) ((size_t) ptr - SIZE_HEAP_DATA);
+//	if (h == NULL)
+//		return malloc(size);
+
+	if (size) {
+		if((h->size - SIZE_HEAP_DATA) < size) {
+			tmp = malloc(size);
+			memcpy(tmp,ptr,h->size - SIZE_HEAP_DATA);
+			free(ptr);
+		} else {
+			tmp = ptr;
+		}
+		return tmp;
+
+	} else {
+		free(ptr);
+	}
+
+	return NULL;
 }
